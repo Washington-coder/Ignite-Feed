56e8608739bd542016a4d194c4d1cf19b3772b31
Washington Antonio <was.antonio.xy@gmail.com>
2022-11-24 10:28:16 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Post } from "./Post"
+
+import "./styles.css"
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Washington"
+        content="loremaldjslfjslfkasjdfçksaçdfsdçflsadçfkaçsdkfjçlkdsalkflaksdlkfalsjdlfkjlaskjdflkjaslkdjflkjasldkjflkasjldkfjlçasdjç"
+      />
+
+    </div>
+  )
+}
