d4fe66b66949afe7ba9679aa04b05a3791c8d3e7
Washington Antonio <was.antonio.xy@gmail.com>
2022-11-24 11:12:17 -0400

    fonts and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
-import "./styles.css"
+import "./global.css"
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Washington"
         content="loremaldjslfjslfkasjdfçksaçdfsdçflsadçfkaçsdkfjçlkdsalkflaksdlkfalsjdlfkjlaskjdflkjaslkdjflkjasldkjflkasjldkfjlçasdjç"
